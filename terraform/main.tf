3360a415a458dee4c367ab3faf18be634441d068
Joao Ferreira <joaobizuca10@gmail.com>
2025-06-23 12:57:21 -0300

    fix: fixing terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,5 @@
-resource "aws_iam_role" "lambda_exec" {
-  name = "devport_lambda_exec"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action    = "sts:AssumeRole",
-      Effect    = "Allow",
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-}
-
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
-  role       = aws_iam_role.lambda_exec.name
+  role       = "arn:aws:iam::058264063478:role/devport_lambda_exec"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
